e434082b5def2e5c0661a294b62d49db0746269a
league <league@wts14-3.attlocal.net>
2023-08-05 11:22:35 -0700

    happy

--- src/_01_else_if/_5_own_adventure/Adventure.java
+++ src/_01_else_if/_5_own_adventure/Adventure.java
@@ -12,15 +12,38 @@ public class Adventure {
 			JOptionPane.showMessageDialog(null, "Enter F to go forward, enter L to go left ,enter R to go right, and enter B to go backward.");
 			System.exit(0);
 		}
-		
-		
-		
-		
 		if(A.equals("yes")) {
-			JOptionPane.showInputDialog("We are going on a bear hunt do you want to go over around under or through?");
-
+	String B = JOptionPane.showInputDialog("We are going on a bear hunt do you want to go over around under or through the cave?");
+	if(B.equals("over")) {
+		JOptionPane.showMessageDialog(null, "A huge bird swooped over your head and ate you.");	
+		System.exit(0);
+	}
+	if(B.equals("around")) {
+		JOptionPane.showMessageDialog(null, "There was milk and you drank it. You got poisned to death.");	
+		System.exit(0);
+		
+	}
+	if(B.equals("under")) {
+		JOptionPane.showMessageDialog(null, "A mole boxed with you and you got KOed.");	
+		System.exit(0);
+	}
+	if(B.equals("through")) {
+String C =	JOptionPane.showInputDialog("You enter the cave and there are four holes around you which do you choose to go through? Behind only 1 door there is treasure. 1-4");
+if(C.equals("1")) {
+	JOptionPane.showMessageDialog(null, "You fell through a hole to the center of the Earth.");	
+	System.exit(0);
+}
+if(C.equals("2")) {
+	JOptionPane.showMessageDialog(null, "The 2 walls next to you started moving and crushed you.");	
+	System.exit(0);
+	}
+	}
+	if(C.equals("3")) {
+		JOptionPane.showMessageDialog(null, "The 2 walls next to you started moving and crushed you.");	
+		System.exit(0);
 		}
-
+	
+		}
 
 
 
